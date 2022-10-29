a95798f57226a66ef801cad9504db546e43cc024
monther123c <monthertamimi123@gmail.com>
2022-10-29 11:56:59 +0000

    cc2

--- python/code_challenges/linkedlist/challenge02/challenge02.py
+++ python/code_challenges/linkedlist/challenge02/challenge02.py
@@ -1,94 +1,58 @@
-# Write here the code challenge solution
 class Node:
-    """This class creates the node """
-    def __init__(self,value):
-        self.value=value
-        self.next= None
+  def __init__(self,value):
+    self.value=value
+    self.next=None
+    
 
-class linkedlist:
-    """this class append and delete a node"""
+class Linkedlist:
+     
     def __init__(self):
         self.head=None
+        self.links = []
+        self.length = 0
 
-    def append(self,node):
-        """this is responseble to append a node"""
-        if self.head==None:
-            self.head=node
-        else:
-            current=self.head
-            while current.next is not None:
-                current = current.next
-            current.next = node
+    def append(self,value):
+        new_node=Node(value)
+        if self.length ==0:
+            self.head=new_node
+            self.length+=1
+            self.links.append(value)
 
-    def find_middle(self):
-        """This function will return the mid value of the linked list"""
-        counter=0
-        current=self.head
-        while current is not None:
-            prev=current
-            current = current.next
-            counter+=1
-        if counter==1:
-            print("the mid is ",prev.value)
-            return prev.value
-        elif counter==0:
-            print ("the list is empty")
-            return "the list is empty"        
-        elif counter % 2 ==1:    
-           mid=counter/2 + 0.5
-        elif counter % 2 ==0:
-            mid=counter/2+1   
+        else: 
+            currentNode= self.head
+            
+            while currentNode.next != None:
+                currentNode = currentNode.next 
+
+            currentNode.next = new_node
+            self.links.append(value)
+            self.length+=1
+            
+    def toArray(self):
+        result = []
+        currentNode = self.head
+        while currentNode != None:
+                result.append(currentNode.value)
+                currentNode = currentNode.next # None
+        return result
         
-        counter=1
-        current=self.head
-        while current.next is not None:
-            current = current.next
-            counter+=1
-            if counter==mid:
-                print("the mid is ",current.value)
-                return current.value   
+    def removeByNode(self, node : Node):
+         
+        next_node = node.next
+         
+        node.value  = next_node.value
+        node.next = next_node.next
+        
+    def getNode_byValue(self,val):
+        currentNode = self.head
+        wantedNode = None
+        while (currentNode.value != val):
+            currentNode = currentNode.next
+        wantedNode = currentNode
+        return wantedNode
 
-
-    def printAll(self):
-        """this is for printing"""
-        if self.head is None:
-            print("The linked list is empty")
-        else:
-            current = self.head
-            while current is not None:
-                print(current.value)
-                current = current.next
-     
-    def OutTest(self):
-        """this is for testing"""
-        lst=[]
-        if self.head is None:
-            return"The linked list is empty"
-        else:
-            current = self.head
-            while current is not None:
-                lst.append(current.value)
-                current = current.next
-        return lst            
-
-
-#####
-linkedList1 = linkedlist()
-node1 = Node("A")
-linkedList1.append(node1)
-
-node2 = Node("B")
-linkedList1.append(node2)
-
-node3 = Node("C")
-linkedList1.append(node3)
-
-node4 = Node("D")
-linkedList1.append(node4)
-
-node5 = Node("E")
-linkedList1.append(node5)
-
-linkedList1.printAll()        
-
-linkedList1.find_middle()                                     
+    def Get_middle_node(self):
+        current_list = self.toArray()
+        middle_index = len(current_list) // 2
+        middle_value = current_list[middle_index]
+        return self.getNode_byValue(middle_value)
--- python/code_challenges/linkedlist/challenge02/test_challenge02.py
+++ python/code_challenges/linkedlist/challenge02/test_challenge02.py
@@ -1,77 +1,28 @@
-# Write your test here
-from platform import node
-import pytest 
-from challenge02 import Node , linkedlist
+import pytest
+from challenge02 import *
 
-def test_append():
-    run=odd_fun()
-    actual=run.test_fun()
-    expected=["A","B","C","D","E"]
-    assert actual==expected
+def test_middle_node1():
+    lista =Linkedlist()
 
+    lista.append(1)
+    lista.append(2)
+    lista.append(3)
+    lista.append(4)
+    lista.append(5)
 
-def test_one_element():
-    
-    one=linkedlist()
-    node_one=Node("A")
-    one.append(node_one)
-    actual=one.find_middle()
-    expected="A"
-    assert actual==expected
+    actual = lista.Get_middle_node().value
+    expected = 3
+    assert actual == expected
 
+def test_middle_node2():
+    lista =Linkedlist()
 
-def test_odd():
-    run=odd_fun()
-    
-    actual=run.find_middle()
-    expected="C"
-    assert actual==expected
+    lista.append(1)
+    lista.append(2)
+    lista.append(3)
+    lista.append(4)
+    lista.append(5)
 
-def test_even():
-    run=even_fun()
-    actual=run.find_middle()
-    expected="C"
-    assert actual==expected
-    
-def test_empty():
-    empty=linkedlist()
-
-    actual= empty.find_middle()
-
-    expected="the list is empty"
-    assert actual==expected
-
-def odd_fun():
-    linkedList1 = linkedlist()
-    node1 = Node("A")
-    linkedList1.append(node1)
-
-    node2 = Node("B")
-    linkedList1.append(node2)
-
-    node3 = Node("C")
-    linkedList1.append(node3)
-
-    node4 = Node("D")
-    linkedList1.append(node4)
-
-    node5 = Node("E")
-    linkedList1.append(node5)
-
-    return linkedList1
-
-def even_fun():
-    linkedList1 = linkedlist()
-    node1 = Node("A")
-    linkedList1.append(node1)
-
-    node2 = Node("B")
-    linkedList1.append(node2)
-
-    node3 = Node("C")
-    linkedList1.append(node3)
-
-    node4 = Node("D")
-    linkedList1.append(node4)
-
-    return linkedList1        
+    actual = lista.Get_middle_node().next.value
+    expected = 4
+    assert actual == expected
