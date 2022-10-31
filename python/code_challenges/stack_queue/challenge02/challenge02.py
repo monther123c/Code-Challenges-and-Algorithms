d9ea932be340e459079010f0ca86e2642d9c9b39
monther123c <monthertamimi123@gmail.com>
2022-10-31 07:10:22 +0000

    CC02

---
+++ python/code_challenges/stack_queue/challenge02/challenge02.py
@@ -0,0 +1,28 @@
+# Write here the code challenge solution
+def isValid(s: str) -> bool:
+    opening = '({[' 
+    closings = ')}]'
+    if s[0] not in opening:
+        return False
+    
+ 
+
+    full = ['()','{}', '[]']          
+    stack = []
+    
+    for ch in s:
+        if ch not in opening and ch not in closings:
+            continue
+            
+        if ch in opening:
+            stack.append(ch)
+        else:
+                
+            if len(stack) == 0:
+                return False
+                
+            if (stack.pop()+ch) not in full:
+                return False
+        
+        
+    return len(stack) == 0
